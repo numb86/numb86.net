593ec89de4059f24804cddffbfe464ec4b87fb78
numb86 <86numb@gmail.com>
2018-11-25 01:32:24 +0900

    GAの設定を行った。

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-36619548-2",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
+  ],
+}
