8aef1935e115805ab29c22dc6410942a0cc6a46e
numb86 <86numb@gmail.com>
2018-11-25 02:27:51 +0900

    トップページを作成した。

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,29 @@
-import React from "react"
+import React from "react";
+import { Link } from "gatsby";
 
-export default () => <div>Hello world!</div>
+const LINK_LIST = [
+  { text: "技術ブログ", url: "http://numb86-tech.hatenablog.com/" },
+  { text: "Twitter", url: "https://twitter.com/numb_86" },
+  { text: "GitHub", url: "https://github.com/numb86" },
+  {
+    text: "例のアレ",
+    url: "http://www.amazon.co.jp/gp/registry/wishlist/V3YG7ZRGFC8U/"
+  }
+];
+
+export default () => (
+  <>
+    <h1>
+      <Link to="/">numb86.net</Link>
+    </h1>
+    <ul>
+      {LINK_LIST.map((l, index) => (
+        <li key={index}>
+          <a href={l.url} target="_blank" rel="noopener noreferrer">
+            {l.text}
+          </a>
+        </li>
+      ))}
+    </ul>
+  </>
+);
