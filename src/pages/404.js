2ba1167665f291af631eff7bf534834f90dba8bd
numb86 <86numb@gmail.com>
2018-11-25 02:31:24 +0900

    404ページを作成した。

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export default () => (
+  <>
+    <h1>
+      <Link to="/">numb86.net</Link>
+    </h1>
+    <h2>404</h2>
+    <p>Not found .</p>
+  </>
+);
