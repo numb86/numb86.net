727a0b63a8960725ec8fc66d1394c856299e9b56
numb86 <86numb@gmail.com>
2018-11-26 22:44:42 +0900

    fix lint

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React from 'react';
 
-import Header from "../components/header";
+import Header from '../components/header';
 
 const LINK_LIST = [
-  { text: "技術ブログ", url: "http://numb86-tech.hatenablog.com/" },
-  { text: "Twitter", url: "https://twitter.com/numb_86" },
-  { text: "GitHub", url: "https://github.com/numb86" },
+  {text: '技術ブログ', url: 'http://numb86-tech.hatenablog.com/'},
+  {text: 'Twitter', url: 'https://twitter.com/numb_86'},
+  {text: 'GitHub', url: 'https://github.com/numb86'},
   {
-    text: "例のアレ",
-    url: "http://www.amazon.co.jp/gp/registry/wishlist/V3YG7ZRGFC8U/"
-  }
+    text: '例のアレ',
+    url: 'http://www.amazon.co.jp/gp/registry/wishlist/V3YG7ZRGFC8U/',
+  },
 ];
 
 export default () => (
   <>
     <Header />
     <ul>
-      {LINK_LIST.map((l, index) => (
-        <li key={index}>
+      {LINK_LIST.map(l => (
+        <li key={l.url}>
           <a href={l.url} target="_blank" rel="noopener noreferrer">
             {l.text}
           </a>
