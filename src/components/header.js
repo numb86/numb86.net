ac6132eeb935dfb1f715838673af448a393c88f7
numb86 <86numb@gmail.com>
2019-03-12 00:37:26 +0900

    ヘッダーに Let's get arrested Badge を貼った。

--- src/components/header.js
+++ src/components/header.js
@@ -4,5 +4,16 @@ import {Link} from 'gatsby';
 export default () => (
   <h1>
     <Link to="/">numb86.net</Link>
+    {` `}
+    <a
+      href="https://github.com/hamukazu/lets-get-arrested"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src="https://img.shields.io/badge/Let's%20get-arrested-red.svg"
+        alt="Let's get arrested badge"
+      />
+    </a>
   </h1>
 );
