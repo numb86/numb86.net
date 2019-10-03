c67b1011cee89a464a0969a71d82f69bbb3ae545
numb86 <16703337+numb86@users.noreply.github.com>
2019-10-04 02:51:32 +0900

    chore: remove wish list

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,6 @@ const LINK_LIST = [
   {text: '技術ブログ', url: 'https://numb86-tech.hatenablog.com/'},
   {text: 'Twitter', url: 'https://twitter.com/numb_86'},
   {text: 'GitHub', url: 'https://github.com/numb86'},
-  {
-    text: '例のアレ',
-    url: 'http://www.amazon.co.jp/gp/registry/wishlist/V3YG7ZRGFC8U/',
-  },
 ];
 
 export default () => (
