9ab5a0d74787180b314320eaaaa0fc918415a264
numb86 <16703337+numb86@users.noreply.github.com>
2020-01-18 06:57:44 +0900

    feat: 動作確認用の Cookie をセットする (#18)
    * $ yarn upgrade react@latest react-dom@latest
    
    * $ yarn add react-cookie
    
    * feat: 動作確認用の Cookie をセットするようにした

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Helmet} from 'react-helmet';
+import {CookiesProvider, useCookies} from 'react-cookie';
 
 import Header from '../components/header';
 
@@ -11,23 +12,32 @@ const LINK_LIST = [
   {text: 'GitHub', url: 'https://github.com/numb86'},
 ];
 
-export default () => (
-  <>
-    <Helmet>
-      <meta charSet="utf-8" />
-      <title>numb86.net</title>
-    </Helmet>
-    <Header />
-    <img src={avator} alt="アバター" />
-    <h2>Link</h2>
-    <ul>
-      {LINK_LIST.map(l => (
-        <li key={l.url}>
-          <a href={l.url} target="_blank" rel="noopener noreferrer">
-            {l.text}
-          </a>
-        </li>
-      ))}
-    </ul>
-  </>
-);
+export default () => {
+  const [, setCookie] = useCookies([]);
+
+  useEffect(() => {
+    setCookie('noSpecifyDomainCookie', '111', {maxAge: 20});
+    setCookie('specifyDomainCookie', '222', {maxAge: 20, domain: 'numb86.net'});
+  }, []);
+
+  return (
+    <CookiesProvider>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>numb86.net</title>
+      </Helmet>
+      <Header />
+      <img src={avator} alt="アバター" />
+      <h2>Link</h2>
+      <ul>
+        {LINK_LIST.map(l => (
+          <li key={l.url}>
+            <a href={l.url} target="_blank" rel="noopener noreferrer">
+              {l.text}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </CookiesProvider>
+  );
+};
